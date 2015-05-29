5365317d5524b61b5a44f2c79bcde4c7f27aed39
NguyenTanDuc <nguyen.tan.duc@framgia.com>
2015-05-29 14:19:34 +0700

    Heroku app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,15 +38,5 @@ Rails.application.configure do
   config.assets.precompile += %w(exam_warning.js)
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = {host: 'localhost:3000'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: ENV["EMAIL"],
-      password: ENV["PASSWORD"]
-
-  }
+ 
 end
